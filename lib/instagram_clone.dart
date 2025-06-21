bf6d3a81d6854d62040c2c9573693b49cef7eed7
DaveAnderson256 <ssembatyadavid54@gmail.com>
2025-06-21 08:19:21 -0400

    Update gradle wrapper to version 8.4 and enhance Instagram clone with image and name lists

--- lib/instagram_clone.dart
+++ lib/instagram_clone.dart
@@ -1,5 +1,7 @@
 // ignore_for_file: prefer_const_constructors, avoid_unnecessary_containers, prefer_const_literals_to_create_immutables, sized_box_for_whitespace
 
+//import 'dart:html';
+
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/rendering.dart';
@@ -15,6 +17,40 @@ class ProfilePageWidget extends StatefulWidget {
 class _ProfilePageWidgetState extends State<ProfilePageWidget> {
   @override
   Widget build(BuildContext context) {
+    List<String> img_list = [
+      "https://www.shutterstock.com/image-photo/very-random-pose-asian-men-260nw-2423213775.jpg",
+      "https://www.shutterstock.com/image-photo/portrait-indonesian-man-long-hair-260nw-2511582609.jpg",
+      "https://www.shutterstock.com/image-photo/these-some-random-photos-260nw-2402066699.jpg",
+      'https://64.media.tumblr.com/db77f82e75871b41c74fada988433425/tumblr_mk4lnhRAJY1rlmi5yo1_1280.jpg',
+      'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRCJa4_mV1FWFgxgQUzBNHrD7ROTf7hoJdzvg&s',
+      'https://thumbs.dreamstime.com/b/obesicat-garden-random-image-fat-pussy-cat-dressed-as-soccer-player-dutch-national-team-exercising-spring-87947898.jpg',
+      'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ6YHPo3kQFRadwI2qD1XnE4oZzIDlVmMq-ag&s',
+      'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR0V8X361Pay5jeM0zYX4C060U36kLOpg8Gi88sL62tvAT5Cj35Y4Rmt8CBVNpRmHkvpkE&usqp=CAU',
+      'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQtqpzv4bbSGNxumH0hMiS6ESKL-kguRYGyyjgAOmQW9ut7orT4spUoeuh9LgXgG77JRwI&usqp=CAU',
+      'https://millbryhill.co.uk/cdn/shop/products/1580723808-00421400_dd785d0e-f4ab-4ace-9a90-27cd55aa8912_1800x1800.jpg?v=1612352475',
+    ];
+    List<String> name_list = [
+      "Alex",
+      "Brian",
+      "Cathy",
+      "Derek",
+      "Ella",
+      "Fiona",
+      "George",
+      "Hannah",
+      "Ivan",
+      "Julia",
+    ];
+
+    // Combine names and images into a list of maps
+    List<Map<String, String>> profiles = List.generate(
+      img_list.length,
+      (index) => {
+        'name': name_list[index],
+        'img': img_list[index],
+      },
+    );
+
     return Scaffold(
       appBar: AppBar(
         leading: Icon(Icons.arrow_back_ios_new),
@@ -99,6 +135,7 @@ class _ProfilePageWidgetState extends State<ProfilePageWidget> {
                                   decoration: BoxDecoration(
                                       shape: BoxShape.circle,
                                       border: Border.all(
+                                        //width: ,
                                         color: Colors.blue,
                                       )),
                                   child: Icon(Icons.arrow_drop_down_sharp),
@@ -111,24 +148,73 @@ class _ProfilePageWidgetState extends State<ProfilePageWidget> {
                     ],
                   ),
                   Container(
+                    color: Colors.grey,
                     height: 100,
-                    child: ListView.builder(
-                      itemCount: 10,
-                      scrollDirection: Axis.horizontal,
-                      itemBuilder: (context, index) {
-                        return Column(
-                          children: [
-                            Container(
-                              padding: EdgeInsets.all(5),
-                              width: 60,
-                              height: 60,
-                              decoration: BoxDecoration(
-                                  shape: BoxShape.circle, color: Colors.red),
-                            ),
-                            Text('Data')
-                          ],
-                        );
-                      },
+                    child: Row(
+                      crossAxisAlignment: CrossAxisAlignment.center,
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        Container(
+                          child: Column(
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            children: [
+                              Container(
+                                margin: EdgeInsets.only(
+                                    top: 25, bottom: 3, left: 5, right: 5),
+                                height: 50,
+                                width: 50,
+                                decoration: BoxDecoration(
+                                  color: Colors.red,
+                                  shape: BoxShape.circle,
+                                ),
+                                child: Icon(
+                                  Icons.add,
+                                  color: Colors.white,
+                                ),
+                              ),
+                              Text(
+                                'Add',
+                                style: TextStyle(fontWeight: FontWeight.bold),
+                              ),
+                            ],
+                          ),
+                        ),
+                        Expanded(
+                          child: ListView.builder(
+                            itemCount: 10,
+                            scrollDirection: Axis.horizontal,
+                            itemBuilder: (context, index) {
+                              print(
+                                  'Image: ${profiles[index]['image']} Name: ${profiles[index]['name']}');
+                              return Column(
+                                mainAxisAlignment: MainAxisAlignment.center,
+                                children: [
+                                  Container(
+                                    margin: EdgeInsets.only(right: 5, top: 18),
+                                    width: 60,
+                                    height: 60,
+                                    decoration: BoxDecoration(
+                                        image: DecorationImage(
+                                            image: NetworkImage(
+                                                profiles[index]['image']!)),
+                                        border: Border.all(
+                                          //width: 65,
+                                          color: Colors.blue,
+                                        ),
+                                        shape: BoxShape.circle,
+                                        color: Colors.red),
+                                  ),
+                                  Text(
+                                    'name',
+                                    style:
+                                        TextStyle(fontWeight: FontWeight.w600),
+                                  )
+                                ],
+                              );
+                            },
+                          ),
+                        ),
+                      ],
                     ),
                   )
                 ],
@@ -138,12 +224,28 @@ class _ProfilePageWidgetState extends State<ProfilePageWidget> {
               height: 80,
               color: const Color.fromARGB(255, 238, 244, 54),
             ),
+
+            //=================for the images=================//
             Expanded(
-              child: Text('data'),
+              child: GridView.builder(
+                  itemCount: img_list.length,
+                  gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                      crossAxisCount: 4),
+                  itemBuilder: ((context, index) {
+                    return Container(
+                      height: 100,
+                      width: 100,
+                      decoration: BoxDecoration(
+                          image: DecorationImage(
+                              fit: BoxFit.cover,
+                              image: NetworkImage(img_list[index]))),
+                    );
+                  })),
             ),
           ],
         ),
       ),
+      //bottomNavigationBar: NavigationBar(),
     );
   }
 
